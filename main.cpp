c45e19ce200382acaf1402df3b131185e4e2b25e
William Tan <tanw@lnxsrv09.seas.ucla.edu>
2018-04-04 21:02:55 -0700

    Detailed output feature

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <string>
 #include <queue>
-
+using namespace std;
 std::string doubleToString(double d)
 {
 	std::ostringstream streamObj3;
@@ -19,16 +19,8 @@ std::string doubleToString(double d)
 	return str;
 }
 
-int main()
+void getInputs(string & source, string & destination, bool & detailed, Navigator & n)
 {
-	using namespace std;
-	cout << "Loading Map Data\n";
-	std::queue<std::string> directions;
-	Navigator n;
-	n.loadMapData("mapdata.txt");
-
-	vector<NavSegment> vn;
-	string source, destination;
 	cout << "Please enter a starting point:\n";
 	getline(cin, source);
 	if (n.attractionPresent(source) == false)
@@ -38,67 +30,116 @@ int main()
 	}
 	cout << "Please enter a destination:\n";
 	getline(cin, destination);
-
 	if (n.attractionPresent(destination) == false)
 	{
 		cout << "Invalid destination\n";
 		exit(1);
 	}
 
+	
+	string details;
+	cout << "Would you like a detailed directions?(Answer yes/no)\n";
+	getline(cin, details);
+	for (int i=0; i < details.size(); i++)
+	{
+		details[i] = tolower(details[i]);
+	}
+	if (details == "yes")
+		detailed = true;
+	else if (details == "no")
+		detailed = false;
+	else
+	{
+		cout << "You didn't put yes or no\n";
+		exit(1);
+	}
 	if (source == destination)
 	{
 		cout << "You've reached your destination!\n";
-		return 0;
+		return;
 	}
-	n.navigate(source, destination, vn);
+}
 
+void direct(const vector<NavSegment> & vn, bool detailed)
+{
+	std::queue<std::string> directions;
 	double rdDist = 0;
 	string prevDir = vn[0].m_direction;
 	bool justTurned = false;
-	for (int i = 0; i < vn.size(); i++)
+	if (detailed == false)
 	{
-		int turn = vn[i].m_command;
-		string curStr;
-		if (turn == 0)
+		for (int i = 0; i < vn.size(); i++)
 		{
-			//rdDist += vn[i].m_distance;
-			string curDir = vn[i].m_direction;
-			if (vn[i].m_direction == prevDir || justTurned == true)
+			int turn = vn[i].m_command;
+			string curStr;
+			if (turn == 0)
 			{
+				string distr = doubleToString(rdDist);
 				rdDist += vn[i].m_distance;
-				if (justTurned)
-					prevDir = curDir;
 			}
 			else
 			{
 				string distr = doubleToString(rdDist);
-				curStr = "Proceed " + prevDir + " on " + vn[i].m_streetName + " for " + distr + " miles\n";
-				rdDist = vn[i].m_distance;
+				curStr = "Proceed on " + vn[i - 1].m_streetName + " for " + distr + " miles\n";
+				rdDist = 0;
 				directions.push(curStr);
-				prevDir = curDir;
+				curStr = "Turn ";
+				curStr += vn[i].m_direction + " on " + vn[i].m_streetName + "\n";
+				directions.push(curStr);
 			}
-			justTurned = false;
+			if (i == vn.size() - 1)
+			{
+				string distr = doubleToString(rdDist);
+				curStr = "Proceed on " + vn[i].m_streetName + " for " + distr + " miles\nYou've reached your destination!\n";
+				directions.push(curStr);
+			}
 		}
-		else if (turn == 1)
+	}
+	else {
+		for (int i = 0; i < vn.size(); i++)
 		{
-			justTurned = true;
-			string distr = doubleToString(rdDist);
-			curStr = "Proceed " + prevDir + " on " + vn[i - 1].m_streetName + " for " + distr + " miles\n";
-			prevDir = "";
-			rdDist = 0;
-			directions.push(curStr);
-			curStr = "Turn ";
-			curStr += vn[i].m_direction + " on " + vn[i].m_streetName + "\n";
-			directions.push(curStr);
+			int turn = vn[i].m_command;
+			string curStr;
+			if (turn == 0)
+			{
+				//rdDist += vn[i].m_distance;
+				string curDir = vn[i].m_direction;
+				if (vn[i].m_direction == prevDir || justTurned == true)	//check if the previous direction is same as current dirrection, if so, keep incrementing
+				{
+					rdDist += vn[i].m_distance;
+					if (justTurned)
+						prevDir = curDir;
+				}
+				else													//else must output the total distance must travel in this certain direction in 1 line
+				{
+					string distr = doubleToString(rdDist);
+					curStr = "Proceed " + prevDir + " on " + vn[i].m_streetName + " for " + distr + " miles\n";
+					rdDist = vn[i].m_distance;
+					directions.push(curStr);
+					prevDir = curDir;
+				}
+				justTurned = false;
+			}
+			else if (turn == 1)											//if turn, must output the direction and distance traveled in 1 line, and output the turn in another line
+			{
+				justTurned = true;
+				string distr = doubleToString(rdDist);
+				curStr = "Proceed " + prevDir + " on " + vn[i - 1].m_streetName + " for " + distr + " miles\n";
+				prevDir = "";
+				rdDist = 0;
+				directions.push(curStr);
+				curStr = "Turn ";
+				curStr += vn[i].m_direction + " on " + vn[i].m_streetName + "\n";
+				directions.push(curStr);
+			}
+			if (i == vn.size() - 1)
+			{
+				string distr = doubleToString(rdDist);
+				curStr = "Proceed " + prevDir + " on " + vn[i].m_streetName + " for " + distr + " miles\nYou've reached your destination!\n";
+				directions.push(curStr);
+			}
+
 		}
-		if (i == vn.size() - 1)
-		{
-			string distr = doubleToString(rdDist);
-			curStr = "Proceed " + prevDir + " on " + vn[i].m_streetName + " for " + distr + " miles\nYou've reached your destination!\n";
-			directions.push(curStr);
-		}
-		/*curStr+= vn[i].m_direction + " on " + vn[i].m_streetName ;		//detailed version
-		*/
 	}
 	int queueSize = directions.size();
 	for (int i = 0; i < queueSize; i++)
@@ -106,4 +147,21 @@ int main()
 		cout << directions.front();
 		directions.pop();
 	}
+}
+
+int main()
+{
+
+	cout << "Loading Map Data\n";
+
+	Navigator n;
+	n.loadMapData("mapdata.txt");
+
+	vector<NavSegment> vn;
+	string source, destination;
+	bool detailed;
+	getInputs(source, destination, detailed, n);
+
+	n.navigate(source, destination, vn);
+	direct(vn,detailed);
 }
